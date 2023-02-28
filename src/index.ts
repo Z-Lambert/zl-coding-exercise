0da2b47c68112606f29fb25b3f0422de20aa59df
Zac Lambert <zaclambert2@gmail.com>
2023-02-28 13:00:18 +0000

    Update fetchAllTimes

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,20 @@ import {
 } from './services';
 require('dotenv').config();
 
-getSunriseAndDayLength();
-
 async function getSunriseAndDayLength() {
   // Generate a list of 100 random lat/long points around the world
-  const no_of_reqs = parseInt(process.env.REQUEST_NUMBER!);
-  const random_list = generateRandomPoints(no_of_reqs);
-  // fetch the data for each lat/long, five at a time
-  const response = await fetchAllTimes(random_list);
-  // Find the response with the earliest sunrise
-  const earliest_day_length = returnEarliestSunrise(response);
-  console.log('earliest sunrise : ', earliest_day_length.sunrise);
-  console.log('day-length: ', earliest_day_length.day_length);
+  if (!process.env.REQUEST_NUMBER) {
+    console.error('env REQUEST_NUMBER required');
+  } else {
+    const no_of_reqs = parseInt(process.env.REQUEST_NUMBER);
+    const random_list = generateRandomPoints(no_of_reqs);
+    // fetch the data for each lat/long, five at a time
+    const response = await fetchAllTimes(random_list);
+    // Find the response with the earliest sunrise
+    const earliest_day_length = returnEarliestSunrise(response);
+    console.log('earliest sunrise : ', earliest_day_length?.sunrise);
+    console.log('day-length: ', earliest_day_length?.day_length);
+  }
 }
+
+getSunriseAndDayLength();
--- src/services.ts
+++ src/services.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
 import { AxiosInstanceClass } from './axios';
+import { chunk } from 'lodash';
 
 require('dotenv').config();
 
@@ -28,107 +28,101 @@ interface Results {
   astronomical_twilight_end: string;
 }
 
-function getTime(...filter: any[]): Promise<Response> | undefined {
-  const url = process.env.SUNSET_URL + filter.filter((x) => x).join('&');
-
-  try {
-    const res = AxiosInstanceClass.getInstance().request({
-      url: url,
-      method: 'get',
-    });
-    return res;
-  } catch (err) {
-    console.error(err);
+function getTime(...filterOptions: string[]): Promise<Response> | undefined {
+  if (!process.env.SUNSET_URL) {
+    console.error('env SUNSET_URL required');
+  } else {
+    // Joining filter options onto the end of the API URL
+    const url =
+      process.env.SUNSET_URL + filterOptions.filter((x) => x).join('&');
+    try {
+      return AxiosInstanceClass.getInstance().request({
+        url,
+        method: 'get',
+      });
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
 
+const LAT_MIN = 90;
+const LAT_MAX = 180;
+const LONG_MIN = 180;
+const LONG_MAX = 360;
+const POINT_DECIMAL_PLACES = 7;
+
 export function generateRandomPoints(n: number): Points[] {
-  const listOfTimes = [...Array(n)].map((_, i) => {
-    const points = {
-      lat: 'lat=' + (Math.random() * 181 - 90).toFixed(7),
-      lng: 'lng=' + (Math.random() * 361 - 160).toFixed(7),
+  return [...Array(n)].map(() => {
+    return {
+      lat:
+        'lat=' +
+        (Math.random() * (LAT_MAX + 1) - LAT_MIN).toFixed(POINT_DECIMAL_PLACES),
+      lng:
+        'lng=' +
+        (Math.random() * (LONG_MAX + 1) - LONG_MIN).toFixed(
+          POINT_DECIMAL_PLACES
+        ),
     };
-    return points;
   });
-  return listOfTimes;
 }
 
-export function returnEarliestSunrise(response: Results[]): Results {
-  return response.reduce((acc: Results | number, curr) => {
-    if (acc == 0) {
+export function returnEarliestSunrise(response: Results[]): Results | null {
+  return response.reduce((acc: Results | null, curr) => {
+    if (acc === null) {
       return curr;
     }
-    const currDate = convertTime12hto24h(curr.sunrise);
-    const accDate = convertTime12hto24h((acc as Results).sunrise);
-    if (currDate < accDate) {
-      return curr as Results;
+    const curr_24 = convertTime12hto24h(curr.sunrise);
+    const acc_24 = convertTime12hto24h(acc.sunrise);
+    if (curr_24 < acc_24) {
+      return curr;
     } else {
-      return acc as Results;
+      return acc;
     }
-  }, response[0]);
+  }, null);
 }
 
 const convertTime12hto24h = (time12h: string): string => {
   const [time, modifier] = time12h.split(' ');
-
   let [hours, minutes, seconds] = time.split(':');
 
   if (hours === '12') {
     hours = '00';
   }
-
   if (modifier === 'PM') {
     hours = (parseInt(hours, 10) + 12).toString();
   }
-
   return `${hours}:${minutes}:${seconds}`;
 };
 
-export async function fetchAllTimes(random_list: Points[]) {
-  const response: Results[] = new Array();
-  // Fetch sunrise/sunset times for all points, but never run more than 5 in parallel
-  while (random_list.length > 0) {
-    // make 5 requests, iterating through the aray randomList using it to build the parameters for getTimes
-    const req1 = random_list[0]
-      ? getTime(random_list[0].lat, random_list[0].lng)
-      : null;
-    const req2 = random_list[1]
-      ? getTime(random_list[1].lat, random_list[1].lng)
-      : null;
-    const req3 = random_list[2]
-      ? getTime(random_list[2].lat, random_list[2].lng)
-      : null;
-    const req4 = random_list[3]
-      ? getTime(random_list[3].lat, random_list[3].lng)
-      : null;
-    const req5 = random_list[4]
-      ? getTime(random_list[4].lat, random_list[4].lng)
-      : null;
-    random_list.splice(0, 5);
-
-    //Resolve the promise of the five requests, passed in as an array
-    const [res1, res2, res3, res4, res5] = await Promise.all([
-      req1,
-      req2,
-      req3,
-      req4,
-      req5,
-    ]);
-    if (res1) {
-      response.push(res1?.data.results);
-    }
-    if (res2) {
-      response.push(res2?.data.results);
-    }
-    if (res3) {
-      response.push(res3?.data.results);
-    }
-    if (res4) {
-      response.push(res4?.data.results);
-    }
-    if (res5) {
-      response.push(res5?.data.results);
-    }
-  }
-  return response;
-}
+export const fetchAllTimes = async (
+  points: Points[],
+  batch_size = 5
+): Promise<Results[]> => {
+  // convert array of all points into batches based on batchSize
+  const batches = chunk(points, batch_size);
+  // One batch at a time, get the times, reducing into one array of results at the end
+  return batches.reduce<Promise<Results[]>>(
+    async (prev_results, batch_points) => {
+      const awaited_prev_result = await prev_results;
+      // Get responses in a batch
+      const responses = await Promise.all(
+        batch_points.map((point) => getTime(point.lat, point.lng))
+      );
+      // Return all in one array, filtering out any that didn't get a response
+      return [
+        ...awaited_prev_result,
+        // Could log instead of filtering out, or error handle in some way
+        ...responses.reduce<Results[]>(
+          (prev_responses, response) => [
+            ...prev_responses,
+            ...(response ? [response.data.results] : []),
+          ],
+          []
+        ),
+      ];
+    },
+    // Initialise with promise of empty array
+    Promise.resolve([])
+  );
+};
